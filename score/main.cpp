444292a16cf15d1e64bc09fcf11f3d55eaa20763
20161104589 <yangzhiwei@ciecimac17deiMac.local>
2018-06-29 15:34:03 +0800

    增加了查找 修改

--- score/main.cpp
+++ score/main.cpp
@@ -5,19 +5,24 @@
 //  Created by 杨志伟 on 18/6/25.
 //  Copyright © 2018年 杨志伟. All rights reserved.
 //
-
-#include <iostream>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<cstring>
+extern int m;
 void calCompetitor(int c,int j);
 void calRater(int a,int b);
 void calScore(int n);//定义评分过程的函数
 struct Compe
 {
+    int num;
     char name;
     char sex;
     char programnaem;
     char programsign;
     char clas;
     int  phone;
+    float aver;
 }compe[10];
 struct Rater
 {
@@ -30,9 +35,11 @@ void menu()
     printf("         ---------比赛打分系统------------\n");
     printf("         ---------1录入评委信息-----------\n");
     printf("         ---------2录入选手信息-----------\n");
-    printf("         ---------3现场打分  ------------\n");
-    printf("         ---------          -----------\n");
-    printf("         ---------          -----------\n");
+    printf("         ---------3  现场打分 ------------\n");
+    printf("         ---------4显示选手信息-----------\n");;
+    printf("         ---------5查找选手信息-----------\n");
+    printf("         ---------6修改选手信息-----------\n");
+    printf("         ---------7删除选手信息-----------\n");
 }
 
 void calRater(int a,int b)
@@ -56,27 +63,6 @@ void calRater(int a,int b)
     }
     
 }
-int main()
-{
-    menu();
-    int e;
-    scanf("%d",&e);
-    switch (e) {
-        case 1:
-            void calRater();
-            break;
-        case 2:
-            void calCompetitor();
-            break;
-        case 3:
-            void calScore();
-            break;
-        default:
-            break;
-    }
-    return 0;
-}
-
 
 void calCompetitor(int c,int j)
 {
@@ -88,6 +74,8 @@ void calCompetitor(int c,int j)
     for(i=0;i<m;i++)
     {
         printf("请输入第%d位选手的信息:\n",c);
+        printf("编号： ");
+        scanf("%d",&compe[j].num);
         printf("姓名：");
         scanf("%s",&compe[j].name);
         printf("性别：");
@@ -105,24 +93,163 @@ void calCompetitor(int c,int j)
     }
     
 }
+
+int main()
+{
+    int e;
+    menu();
+    scanf("%d",&e);
+    switch (e)
+    {
+        case 1:
+            calRater();
+            break;
+        case 2:
+            calCompetitor();
+            break;
+        case 3:
+            calScore();
+            break;
+        case 4:
+            display();
+            break;
+        case 5:
+            inq();
+            break;
+        case 6:
+            mod();
+            break;
+            
+    }
+    return 0;
+}
+
+
+
 void calScore(int n)
 {
-    int i;
+    int i,j;
     double sum=0,max=0,min=10,score,aver;
     printf("请输入选手成绩(0-10)\n");
-    for(i=1;i<=n;i++)
+    for(j=0;j<m;j++)
     {
-        printf("第%d位评委给分：",i);
-        scanf("%lf",&score);
-        sum=sum+score;
-        if(max<score)
-        max=score;
-        if(min>score)
-            min=score;
+        for(i=1;i<=n;i++)
+        {
+            printf("第%d位评委给分：",i);
+            scanf("%lf",&score);
+            sum=sum+score;
+            if(max<score)
+                max=score;
+            if(min>score)
+                min=score;
+        }
+        sum=sum-max-min;
+        printf("减去一个最高分：%.2f,减去一个最低分：%.2f\n",max,min);
+        aver=(sum)/(n-2);
+        printf("当前选手得分：%.3f\n",aver);
+        stpcpy(compe[j].aver, aver);
     }
-    sum=sum-max-min;
-    printf("减去一个最高分：%.2f,减去一个最低分：%.2f\n",max,min);
-    aver=(sum)/(n-2);
-    printf("当前选手得分：%.3f\n",aver);
 
+    
+}
+void display(int m)
+{
+    int i;
+    for(i=0;i<m;i++)
+    {
+        printf("编号：  ");
+        printf("%d",compe[i].num);
+        printf("姓名: ");
+        printf("%s     ",compe[i].name);
+        printf("性别:");
+        printf("%s     ",compe[i].sex);
+        printf("节目名称:");
+        printf("%s     ",compe[i].programnaem);
+        printf("节目类别:     ");
+        printf("%s   ",compe[i].programsign);
+        printf("班级：");
+        printf("%s   ",compe[i].clas);
+        printf("电话：");
+        printf("%d   ",compe[i].phone);
+        printf("平均得分： %d",compe[i].aver);
+    }
+}
+int num;
+void inq()
+{
+    int i,j;
+    int flag,myid,pos;
+    printf("please input inquire id:");
+    scanf("%d",&myid);
+    flag=0;
+    for(i=0;i<num;i++)
+    {
+        if(compe[i].name==myid)
+        {
+            flag=1;
+            break;
+        }
+    }
+    if(flag==0)
+    {
+        printf(" no find this student \n");
+        exit(0);
+    }
+    else
+    {
+        printf("编号：  ");
+        printf("%d",compe[i].num);
+        printf("姓名: ");
+        printf("%s     ",compe[i].name);
+        printf("性别:");
+        printf("%s     ",compe[i].sex);
+        printf("节目名称:");
+        printf("%s     ",compe[i].programnaem);
+        printf("节目类别:     ");
+        printf("%s   ",compe[i].programsign);
+        printf("班级：");
+        printf("%s   ",compe[i].clas);
+        printf("电话：");
+        printf("%d   ",compe[i].phone);
+        printf("平均得分： %d",compe[i].aver);
+    }
+}
+
+void mod()
+{
+    int i,j;
+    int flag,myid,pos;
+    printf("please input modify id:");
+    scanf("%d",&myid);
+    flag=0;
+    for(i=0;i<m;i++)
+    {
+        if(compe[i].num==myid)
+        {
+            flag=1;
+            break;
+        }
+    }
+    if(flag==0)
+    {
+        printf(" no find this student \n");
+        exit(0);
+    }
+    else
+    {
+        printf("编号： ");
+        scanf("%d",&compe[i].num);
+        printf("姓名：");
+        scanf("%s",&compe[i].name);
+        printf("性别：");
+        scanf("%s",&compe[i].sex);
+        printf("节目名称：");
+        scanf("%s",&compe[i].programnaem);
+        printf("节目类别：");
+        scanf("%s",&compe[i].programsign);
+        printf("班级：");
+        scanf("%s",&compe[i].clas);
+        printf("电话：");
+        scanf("%d",&compe[i].phone);
+    }
 }
